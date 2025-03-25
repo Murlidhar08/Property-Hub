f7cc2300c5ce5565bcd3c730a06d2cef26bc0476
Mahesh Chavda <chavdamahesh2772@gmail.com>
2025-03-25 14:32:25 +0530

    PWA and Logo added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,106 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import * as path from 'path';
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server : {
+  server: {
     port: 4000
   },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ["**/*.{js,css,html,png,svg,ico,jpg,json}"],
+      },
+      includeAssets: [
+        "favicon.ico",
+        "images/favicon-16x16.png",
+        "images/favicon-32x32.png"
+      ],
+      manifest: {
+        name: "Property Hub",
+        short_name: "PropertyHub",
+        description: "Organize, Track, and Manage Your Property with Ease.",
+        theme_color: "#ff69b4",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/images/logo/maskable_icon_x48.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/maskable_icon_x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/maskable_icon_x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/maskable_icon_x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/maskable_icon_x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/images/logo/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/images/logo/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "/images/logo/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "/images/logo/favicon.ico",
+            sizes: "48x48",
+            type: "image/x-icon",
+          }
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
-      // eslint-disable-next-line no-undef
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@', replacement: path.resolve(path.dirname(), 'src') },
     ],
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
